224fa13c35f26655bee85299291fbeef9b48f4bf
Tamara <tamara@linumlabs.com>
2022-05-07 16:23:15 +0200

    Updated AppStorage.sol

---
+++ contracts/libraries/AppSotrage.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+struct User {
+    string name;
+    uint96 age; // uint96 = 8 bytes, to pack with 20 byte address below
+    address walletAddress;
+}
+
+// All state shared across the protocol goes here
+struct AppStorage {
+    mapping(uint256 => User) users;
+    uint256 userCount;
+    User oldestUser;
+}
+
+library LibAppStorage {
+    function diamondStorage() internal pure returns (AppStorage storage ds) {
+        assembly {
+            ds.slot := 0
+        }
+    }
+}
