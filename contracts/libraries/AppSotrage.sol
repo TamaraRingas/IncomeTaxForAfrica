bf0abb0ae5dd575b09245c7e247dc34107b6f1f8
Gareth <gareth@cis.za.net>
2022-05-07 16:54:23 +0200

    Updated AppStorage

--- contracts/libraries/AppSotrage.sol
+++ contracts/libraries/AppSotrage.sol
@@ -7,6 +7,65 @@ struct User {
     address walletAddress;
 }
 
+struct Project {
+    uint256 projectID;
+    uint256 sectorID;
+    uint256 projectBudget;
+    ProjectState _projectState;
+    uint256 numberOfVotes;
+    //Out of 10: 10 being high priority
+    uint256 priorityPoints;
+}
+
+struct TaxPayerCompany {
+    uint256 companyID;
+    address wallet;
+    string name;
+}
+
+struct Citizen {
+    uint256 citizenID;
+    uint256 citizenIdNumber;
+    uint256 taxPercentage;
+    uint256 primarySectoryID;
+    uint256 secondarySectorID;
+    uint256 totalTaxPaid;
+
+    //Total taxPaid / 1000
+    uint256 totalPriorityPoints;
+    string firstName;
+    string secondName;
+}
+
+struct Sector {
+    uint256 sectorID;
+    uint256 numberOfProjects;
+    string sectorName;
+}
+
+struct Tender {
+    uint256 tenderID;
+    uint256 projectID;
+    uint256 sectorID;
+    uint256 priceCharged;
+    uint256 numberOfPublicVotes;
+    uint256 numberOfGovernmentVotes;
+    address headOfProject;
+    string companyName;
+}
+
+enum ProjectState {
+    VOTING,
+    APPROVED,
+    DECLINED,
+    TENDERING,
+    TENDER_VOTING,
+    GOVERNMENT_VOTING,
+    AWARDED,
+    DEVELOPMENT,
+    CLOSED
+}
+
 // All state shared across the protocol goes here
 struct AppStorage {
     mapping(uint256 => User) users;
