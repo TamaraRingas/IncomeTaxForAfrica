246a47dfa64648e1e1cf47147b300137ce140749
Tamara Ringas <tamriasdev@gmail.com>
2022-05-16 17:55:34 +0200

    Added events

--- contracts/facets/TreasuryFacet.sol
+++ contracts/facets/TreasuryFacet.sol
@@ -1,7 +1,29 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.13;
 
-contract TreasuryFacet {
+import "./ProposalFacet.sol/";
+import { AppStorage, Modifiers } from "../libraries/AppStorage.sol";
 
+import '@openzeppelin/contracts/access/Ownable.sol';
 
+contract TreasuryFacet is Ownable {
+
+  AppStorage internal s;
+
+  address public owner;
+
+    //----------------------------------------------------------------------------------------------------------------------
+    //-----------------------------------------  EVENTS        ---------------------------------------
+    //----------------------------------------------------------------------------------------------------------------------
+
+  event PhaseOnePaid(uint256 amount, uint256 time);
+  event PhaseOneClosed();
+  event PhaseTwoPaid(uint256 amount, uint256 time);
+  event PhaseTwoClosed();
+  event PhaseThreePaid(uint256 amount, uint256 time);
+  event PhaseThreeClosed();
+  event PhaseFourPaid(uint256 amount, uint256 time);
+  event PhaseFourClosed();
+
+   
 }
