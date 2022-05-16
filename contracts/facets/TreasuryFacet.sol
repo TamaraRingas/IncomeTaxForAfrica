798327c8e76970aaa90575731a2aa4f62ec9624c
Tamara Ringas <tamriasdev@gmail.com>
2022-05-16 17:21:08 +0200

    Function Scaffolding

--- contracts/facets/TreasuryFacet.sol
+++ contracts/facets/TreasuryFacet.sol
@@ -1,7 +1,80 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.13;
 
-contract TreasuryFacet {
+import "./ProposalFacet.sol/";
+import { AppStorage, Modifiers } from "../libraries/AppStorage.sol";
 
+import '@openzeppelin/contracts/access/Ownable.sol';
 
+contract TreasuryFacet is Ownable {
+
+  AppStorage internal s;
+
+  address public owner;
+
+  // ------------------------------------------ //
+	//                  EVENTS                    //
+	// ------------------------------------------ //
+
+  event PhaseOnePaid(uint256 amount, uint256 time);
+  event PhaseOneClosed();
+  event PhaseTwoPaid(uint256 amount, uint256 time);
+  event PhaseTwoClosed();
+  event PhaseThreePaid(uint256 amount, uint256 time);
+  event PhaseThreeClosed();
+  event PhaseFourPaid(uint256 amount, uint256 time);
+  event PhaseFourClosed();
+
+  // ------------------------------------------ //
+	//                 CONSTRUCTOR                //
+	// ------------------------------------------ //
+
+  constructor (address _USDC) {
+    s.USDAddress = _USDC;
+    owner = msg.sender;
+  }
+
+    //----------------------------------------------------------------------------------------------------------------------
+    //-----------------------------------------         GENERAL FUNCTIONALITY        ---------------------------------------
+    //----------------------------------------------------------------------------------------------------------------------
+
+  function payPhaseOne() external onlyAdmin {
+    require(tenderState == DEVELOPMENT, "NOT DEVELOPMENT");
+
+  }
+
+  function closePhaseOne() external onlySupervisor {
+
+  }
+
+  function payPhaseTwo() external onlyAdmin {
+    require(tenderState == DEVELOPMENT, "NOT DEVELOPMENT");
+  }
+
+  function closePhaseTwo() external onlySupervisor {
+
+  }
+
+  function payPhaseThree() external onlyAdmin {
+    require(tenderState == DEVELOPMENT, "NOT DEVELOPMENT");
+  }
+
+  function closePhaseThree() external onlySupervisor {
+    
+  }
+
+  function payPhaseFour() external onlyAdmin {
+    require(tenderState == DEVELOPMENT, "NOT DEVELOPMENT");
+  }
+
+  function closePhaseFour() external onlySupervisor {
+
+  }
+
+  function setSupervisor(address supervisor) internal onlyOwner {
+    s.supervisors[supervisor] = true;
+  }
+
+  
+  
 }
