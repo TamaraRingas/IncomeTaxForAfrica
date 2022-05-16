156a399d5383d3aa6aba140aa87e82b32dc3b377
Tamara Ringas <tamriasdev@gmail.com>
2022-05-16 17:21:19 +0200

    Set owner

--- contracts/facets/GovernanceFacet.sol
+++ contracts/facets/GovernanceFacet.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.13;
 
 import { AppStorage, Modifiers } from "../libraries/AppStorage.sol";
 
-contract GovernanceFacet {
+contract GovernanceFacet is Ownable {
+
   AppStorage internal s;
+
+
+  function setSectorAdmin() is onlyOwner {
+    
+  }
+  
 }
