0db9aee029124f3efbbddae535fd868f54cd2d57
Tamara Ringas <tamriasdev@gmail.com>
2022-05-16 22:29:11 +0200

    impoer IERC20

--- contracts/facets/TreasuryFacet.sol
+++ contracts/facets/TreasuryFacet.sol
@@ -8,6 +8,7 @@ import { AppStorage, Modifiers } from "../libraries/AppStorage.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TreasuryFacet is ITreasuryFacet, Ownable, Modifiers, ReentrancyGuard {
 
