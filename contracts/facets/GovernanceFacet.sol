c86650ef76ea3d2b8b415c274271c1730e082722
Tamara Ringas <tamriasdev@gmail.com>
2022-05-17 09:49:18 +0200

    Added require to check for 0 address

--- contracts/facets/GovernanceFacet.sol
+++ contracts/facets/GovernanceFacet.sol
@@ -60,9 +60,10 @@ contract GovernanceFacet is IGovernanceFacet, Ownable, Modifiers {
         emit ChangeCompanyAdmin(_companyID, previousAdmin, s.companies[_companyID].admin);
   }
   
-  function setSupervisor(uint256 projectID, address supervisor) public onlySupervisor {
-  //   s.supervisors[supervisor] = true;
-  // } 
+  function setSupervisor(uint256 projectID, address _newSupervisor) public onlySupervisor {
+    require(_newSupervisor != address(0), "CANNOT BE ZERO ADDRESS");
+    
+  } 
 
   // function removeSupervisor(address supervisor) internal onlyOwner {
   //   s.supervisors[supervisor] = false;
