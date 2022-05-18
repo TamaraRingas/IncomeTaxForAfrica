75581b891ba5743d0deb4eca056dda3f73704d85
Tamara Ringas <tamriasdev@gmail.com>
2022-05-18 15:10:33 +0200

    Added function getTreasuryBalance()

--- contracts/interfaces/ITreasuryFacet.sol
+++ contracts/interfaces/ITreasuryFacet.sol
@@ -4,6 +4,11 @@ pragma solidity 0.8.13;
 import "../libraries/AppStorage.sol";
 
 interface ITreasuryFacet {
+
+  function getProposalStateDetails(uint256 _proposalID) external;
+
+  function getTreasuryBalance() external;
+
   
-    function getProposalStateDetails(uint256 _proposalID) external;
+
 }
