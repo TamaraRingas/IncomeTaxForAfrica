6715f2daeea81985af9d2ddd56c51a23d2c9c828
Gareth <gareth@cis.za.net>
2022-05-19 22:22:41 +0200

    Updates

--- contracts/contracts/Citizen.sol
+++ contracts/contracts/Citizen.sol
@@ -66,9 +66,8 @@ contract Citizen is ICitizen {
         return citizens[_citizenID];
     }
 
-    function getUserID(uint256 _citizenID) public view returns (uint256){
+    function getUserID(address _citizenID) public view returns (uint256){
         return userAddressesToIDs[_citizenID];
     }
-
     
 }
