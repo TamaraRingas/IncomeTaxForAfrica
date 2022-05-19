e79bc84402b9ba4b0b891f90e372589472d704a8
Tamara Ringas <tamriasdev@gmail.com>
2022-05-19 16:08:46 +0200

    Removed AppStorage import

--- contracts/interfaces/ITender.sol
+++ contracts/interfaces/ITender.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-import "../libraries/AppStorage.sol";
-
 interface ITenderFacet {
 
     function createTender(Tender memory _tender) external;
