9a12fa9c4dd0f30c2dcbcdc1136cdde5db29cb48
Tamara Ringas <tamriasdev@gmail.com>
2022-05-19 16:05:02 +0200

    Removed AppStorage import

--- contracts/interfaces/ISector.sol
+++ contracts/interfaces/ISector.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-import "../libraries/AppStorage.sol";
+interface SectorFacet {
 
-interface ISectorFacet {
-
-    function createSector(string memory _name) external;
+    function createSector(string calldata _name) external;
     
 }
