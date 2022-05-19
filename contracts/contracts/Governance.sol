68520780ce42065d2200095ee5f6e49ac34fbc4a
Gareth <gareth@cis.za.net>
2022-05-19 19:30:14 +0200

    Governance updates, removed s

--- contracts/contracts/Governance.sol
+++ contracts/contracts/Governance.sol
@@ -2,6 +2,12 @@
 pragma solidity 0.8.13;
 
 import "..//interfaces/IGovernance.sol";
+import "./Citizen.sol";
+import "./Proposal.sol";
+import "./Sector.sol";
+import "./Tender.sol";
+import "./TaxPayerCompany.sol";
+import "./Treasury.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
@@ -10,6 +16,15 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract Governance is IGovernance, Ownable, ReentrancyGuard {
 
   address superAdmin;
+  address USDAddress;
+  IERC20 USDC;
+
+  Citizen public _citizen;
+  Proposal public _proposal;
+  Tender public _tender;
+  Sector public _sector;
+  TaxPayerCompany public _company;
+  Treasury public _treasury;
 
     //----------------------------------------------------------------------------------------------------------------------
     //-----------------------------------------  EVENTS        ---------------------------------------
@@ -29,9 +44,9 @@ contract Governance is IGovernance, Ownable, ReentrancyGuard {
     //----------------------------------------------------------------------------------------------------------------------
 
   constructor (address _USDC) {
-    s.USDAddress = _USDC;
-    s.USDC = IERC20(_USDC);
-    s.superAdmin = msg.sender;
+    USDAddress = _USDC;
+    USDC = IERC20(_USDC);
+    superAdmin = msg.sender;
   }
 
     //----------------------------------------------------------------------------------------------------------------------
@@ -41,49 +56,49 @@ contract Governance is IGovernance, Ownable, ReentrancyGuard {
   function setSuperAdmin(address _newSuperAdmin) public onlySuperAdmin(){
         require(_newSuperAdmin != address(0), "CANNOT BE ZERO ADDRESS");
 
-        address previousSuperAdmin = s.superAdmin;
+        address previousSuperAdmin = superAdmin;
 
-        s.superAdmin = _newSuperAdmin;
+        superAdmin = _newSuperAdmin;
 
-        emit SetSuperAdmin(previousSuperAdmin, s.superAdmin, block.timestamp);
+        emit SetSuperAdmin(previousSuperAdmin, superAdmin, block.timestamp);
   }
 
   function setTenderAdmin(uint256 _tenderID, address _admin) public onlySuperAdmin(){
         require(_admin != address(0), "CANNOT BE ZERO ADDRESS");
 
-        address previousAdmin =  s.tenders[_tenderID].admin;
+        address previousAdmin =  _tender.tenders[_tenderID].admin;
 
-        s.tenders[_tenderID].admin = _admin;
+        _tender.tenders[_tenderID].admin = _admin;
 
-        emit SetTenderAdmin(_tenderID, previousAdmin, s.tenders[_tenderID].admin, block.timestamp);
+        emit SetTenderAdmin(_tenderID, previousAdmin, _tender.tenders[_tenderID].admin, block.timestamp);
   }
 
 
   function setSectorAdmin(uint256 _sectorID, address _newAdmin) public onlyOwner {
     require(_newAdmin != address(0), "CANNOT BE ZERO ADDRESS");
 
-    s.sectors[_sectorID].sectorAdmins[_newAdmin] = true; 
+    _sector.sectors[_sectorID].sectorAdmins[_newAdmin] = true; 
 
     emit SetSectorAdmin(_sectorID, _newAdmin, block.timestamp);
   }
 
   function changeCompanyAdmin(uint256 _companyID, address _newAdmin) public onlyAdmin (_companyID) {
         require(_newAdmin != address(0), "CANNOT BE ZERO ADDRESS");
-        require(_companyID <= s.numberOfCompanies, "NOT A VALID COMPANY ID");
+        require(_companyID <= _company.numberOfCompanies, "NOT A VALID COMPANY ID");
 
-        address previousAdmin =  s.companies[_companyID].admin;
-        s.companies[_companyID].admin = _newAdmin;
+        address previousAdmin =  _company.companies[_companyID].admin;
+        _company.companies[_companyID].admin = _newAdmin;
 
-        emit ChangeCompanyAdmin(_companyID, previousAdmin, s.companies[_companyID].admin, block.timestamp);
+        emit ChangeCompanyAdmin(_companyID, previousAdmin, _company.companies[_companyID].admin, block.timestamp);
   }
   
   function setSupervisor(uint256 _proposalID, address _newSupervisor) public onlySupervisor(_proposalID) {
     require(_newSupervisor != address(0), "CANNOT BE ZERO ADDRESS");
-    require(_proposalID <= s.numberOfProposals, "NOT A VALID COMPANY ID");
+    require(_proposalID <= _proposal.numberOfProposals, "NOT A VALID COMPANY ID");
 
-    address previousSupervisor =  s.proposals[_proposalID].supervisor;
+    address previousSupervisor =  _proposal.proposals[_proposalID].supervisor;
 
-    s.proposals[_proposalID].supervisor = _newSupervisor;
+    _proposal.proposals[_proposalID].supervisor = _newSupervisor;
 
     emit SetSupervisor(_proposalID, previousSupervisor, _newSupervisor, block.timestamp);
   } 
@@ -94,39 +109,39 @@ contract Governance is IGovernance, Ownable, ReentrancyGuard {
 
   function fundTreasury(uint256 _amount) public onlySuperAdmin() nonReentrant {
 
-    s.USDC.transfer(s.TreasuryAddress, _amount);
+    USDC.transfer(_treasury.TreasuryAddress, _amount);
 
     emit TreasuryBalanceUpdated(_amount);
   }
 
   function updateBudget(uint256 _sectorID, uint256 _newBudget) public onlySuperAdmin() {
-    s.sectors[_sectorID].budget = _newBudget;
+    _sector.sectors[_sectorID].budget = _newBudget;
 
     emit SectorBudgetUpdated(_newBudget);
   }
 
    modifier onlyAdmin(uint256 _tenderID) {
-        require(msg.sender == s.tenders[_tenderID].admin, "ONLY ADMIN");
+        require(msg.sender == _tender.tenders[_tenderID].admin, "ONLY ADMIN");
         _;
     }
 
     modifier onlySuperAdmin() {
-        require(msg.sender == s.superAdmin, "ONLY SUPER ADMIN");
+        require(msg.sender == superAdmin, "ONLY SUPER ADMIN");
         _;
     }
 
     modifier onlySupervisor(uint256 _proposalID) {
-        require(msg.sender == s.proposals[_proposalID].supervisor, "ONLY SUPERVISOR");
+        require(msg.sender == _proposal.proposals[_proposalID].supervisor, "ONLY SUPERVISOR");
         _;
     }
 
     modifier onlySectorAdmins(uint256 _sectorID) {
-        require(s.sectors[_sectorID].sectorAdmins[msg.sender] == true, "ONLY SECTOR ADMINS");
+        require(_sector.sectors[_sectorID].sectorAdmins[msg.sender] == true, "ONLY SECTOR ADMINS");
         _;
     }
 
      modifier onlyCompanyAdmin(uint256 _companyID) {
-        require(msg.sender == s.companies[_companyID].admin, "ONLY COMPANY ADMIN");
+        require(msg.sender == _company.companies[_companyID].admin, "ONLY COMPANY ADMIN");
         _;
     }
 
