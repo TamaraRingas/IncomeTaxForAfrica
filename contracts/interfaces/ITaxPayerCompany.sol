13d570a7d9a7816da796ec4a5f8b47c1a9bbdccd
Tamara Ringas <tamriasdev@gmail.com>
2022-05-19 16:09:14 +0200

    Removed AppStorage import

--- contracts/interfaces/ITaxPayerCompany.sol
+++ contracts/interfaces/ITaxPayerCompany.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-import "../libraries/AppStorage.sol";
-
 interface ITaxPayerCompanyFacet {
 
     function payEmployeeTax(uint256 _companyID, uint256 _citizenID) external;
