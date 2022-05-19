e792a7808141bc369ad6d4098391b50f1bf4a1fb
Tamara Ringas <tamriasdev@gmail.com>
2022-05-19 16:07:03 +0200

    Renamed interface

--- contracts/interfaces/IProposal.sol
+++ contracts/interfaces/IProposal.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-interface ProposalFacet {
+interface IProposal {
 
     function createProposal(Proposal memory _proposal, address _supervisor) external;
 
