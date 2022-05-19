453b9cfc9427ec81403a9596e085ddebf51361d6
Tamara Ringas <tamriasdev@gmail.com>
2022-05-19 16:05:08 +0200

    Removed AppStorage import

--- contracts/interfaces/IGovernance.sol
+++ contracts/interfaces/IGovernance.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-import "../libraries/AppStorage.sol";
-
 interface IGovernanceFacet {
   
   function setSuperAdmin(address _newSuperAdmin) external;
