03f1c1168e2079adc165f0b691794a21a33e5261
Tamara Ringas <tamriasdev@gmail.com>
2022-05-19 16:28:08 +0200

    Renamed contract

--- contracts/facets/TaxPayerCompany.sol
+++ contracts/facets/TaxPayerCompany.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.13;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../interfaces/ITaxPayerCompany.sol";
 
-contract TaxPayerCompanyFacet is ITaxPayerCompany{
+contract TaxPayerCompany is ITaxPayerCompany {
 
     //TODO events
     //TODO view functions
