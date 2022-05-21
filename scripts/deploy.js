09abc6d179f1cfee3cd55b09cd11b89bb7d9353f
Tamara Ringas <tamriasdev@gmail.com>
2022-05-21 07:19:39 +0200

    Updated deploy script with new contract names

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,79 +1,36 @@
 /* global ethers */
 /* eslint prefer-const: "off" */
 
-const { getSelectors, FacetCutAction } = require('./libraries/diamond.js')
+async function deploy() {
+  const [deployer] = await ethers.getSigners();
 
-async function deployDiamond () {
-  const accounts = await ethers.getSigners()
-  const contractOwner = accounts[0]
+  console.log("Deploying contracts with the account:", deployer.address);
 
-  // deploy DiamondCutFacet
-  const DiamondCutFacet = await ethers.getContractFactory('DiamondCutFacet')
-  const diamondCutFacet = await DiamondCutFacet.deploy()
-  await diamondCutFacet.deployed()
-  console.log('DiamondCutFacet deployed:', diamondCutFacet.address)
+    // Deploy core 
+    GovernanceContract = await ethers.getContractFactory("Governance");
+    GovernanceInstance = await GovernanceContract.connect(owner).deploy(constants.POLYGON.USDC);
 
-  // deploy Diamond
-  const Diamond = await ethers.getContractFactory('Diamond')
-  const diamond = await Diamond.deploy(contractOwner.address, diamondCutFacet.address)
-  await diamond.deployed()
-  console.log('Diamond deployed:', diamond.address)
+    SectorContract = await ethers.getContractFactory("Sector");
+    SectorInstance = await SectorContract.connect(superAdmin).deploy();
 
-  // deploy DiamondInit
-  // DiamondInit provides a function that is called when the diamond is upgraded to initialize state variables
-  // Read about how the diamondCut function works here: https://eips.ethereum.org/EIPS/eip-2535#addingreplacingremoving-functions
-  const DiamondInit = await ethers.getContractFactory('DiamondInit')
-  const diamondInit = await DiamondInit.deploy()
-  await diamondInit.deployed()
-  console.log('DiamondInit deployed:', diamondInit.address)
+    CitizenContract = await ethers.getContractFactory("Citizen");
+    CitizenInstance = await CitizenContract.connect(superAdmin).deploy();
 
-  // deploy facets
-  console.log('')
-  console.log('Deploying facets')
-  const FacetNames = [
-    'DiamondLoupeFacet',
-    'OwnershipFacet'
-  ]
-  const cut = []
-  for (const FacetName of FacetNames) {
-    const Facet = await ethers.getContractFactory(FacetName)
-    const facet = await Facet.deploy()
-    await facet.deployed()
-    console.log(`${FacetName} deployed: ${facet.address}`)
-    cut.push({
-      facetAddress: facet.address,
-      action: FacetCutAction.Add,
-      functionSelectors: getSelectors(facet)
-    })
-  }
+    TenderContract = await ethers.getContractFactory("Tender");
+    TenderInstance = await TenderContract.connect(superAdmin).deploy();
 
-  // upgrade diamond with facets
-  console.log('')
-  console.log('Diamond Cut:', cut)
-  const diamondCut = await ethers.getContractAt('IDiamondCut', diamond.address)
-  let tx
-  let receipt
-  // call to init function
-  let functionCall = diamondInit.interface.encodeFunctionData('init')
-  tx = await diamondCut.diamondCut(cut, diamondInit.address, functionCall)
-  console.log('Diamond cut tx: ', tx.hash)
-  receipt = await tx.wait()
-  if (!receipt.status) {
-    throw Error(`Diamond upgrade failed: ${tx.hash}`)
-  }
-  console.log('Completed diamond cut')
-  return diamond.address
+    ProposalContract = await ethers.getContractFactory("Proposal");
+    ProposalInstance = await ProposalContract.connect(superAdmin).deploy();
+
+    TreasuryContract = await ethers.getContractFactory("Treasury");
+    TreasuryInstance = await TreasuryContract.connect(superAdmin).deploy();
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-if (require.main === module) {
-  deployDiamond()
-    .then(() => process.exit(0))
-    .catch(error => {
-      console.error(error)
-      process.exit(1)
-    })
-}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
 
-exports.deployDiamond = deployDiamond
+exports.deploy = deploy
